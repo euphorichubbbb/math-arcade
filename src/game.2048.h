f9df5ba3cdb15279ed362a0d4c8b33d13dcb435f
euphorichubbbb <106897467+euphorichubbbb@users.noreply.github.com>
2023-01-22 11:48:02 +0530

    Create game.2048.h

---
+++ src/game.2048.h
@@ -0,0 +1,18 @@
+#ifndef game2048_include_file
+#define game2048_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define game2048_width 32
+#define game2048_height 32
+#define game2048_size 1026
+#define game2048 ((gfx_sprite_t*)game2048_data)
+extern unsigned char game2048_data[1026];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
