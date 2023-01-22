472a3a1af05eebf5af99716071ee5d7c8850bf58
euphorichubbbb <106897467+euphorichubbbb@users.noreply.github.com>
2023-01-22 11:48:58 +0530

    Create gfx.h

---
+++ src/gfx.h
@@ -0,0 +1,26 @@
+#ifndef gfx_include_file
+#define gfx_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "global_palette.h"
+#define sprites_palette_offset 0
+#include "sudoku.h"
+#include "snake.h"
+#include "game2048.h"
+#include "sokoban.h"
+#include "sokobox.h"
+#include "sokowall.h"
+#include "sokodest.h"
+#include "sokoup.h"
+#include "sokodown.h"
+#include "sokoleft.h"
+#include "sokoright.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
